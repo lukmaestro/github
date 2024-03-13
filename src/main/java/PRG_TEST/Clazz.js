e8a17e8a0ddcff0425c88e8e5d7a8ffc9e4b92a8
lukmaestro <lukas.eichler1@email.cz>
2024-03-13 08:43:00 +0100

    poslední commit

--- src/main/java/PRG_TEST/Clazz.js
+++ src/main/java/PRG_TEST/Clazz.js
@@ -1,11 +1,48 @@
 class Clazz {
     constructor(nazev) {
         this.nazev = nazev;
+        this.studenti = [];
+    }
+
+    pridejStudenta(student) {
+        student.nazevTridy = this.nazev;
+        this.studenti.push(student);
     }
 }
 
+class Student {
+    constructor(jmeno, prijmeni, vek) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.vek = vek;
+        this.nazevTridy = null;
+    }
+        toString() {
+            return `${this.jmeno}, ${this.prijmeni}, ${this.nazevTridy}`;
+        }
 
-const instance = new Clazz("1A");
-console.log(instance.nazev);
+}
+
+
+const classA = new Clazz("ClassA");
+const classB = new Clazz("ClassB");
+
+
+for (let i = 0; i < 5; i++) {
+    const student = new Student(`Jmeno${i}`, `Prijmeni${i}`, 17);
+    classA.pridejStudenta(student);
+}
+
+
+for (let i = 5; i < 10; i++) {
+    const student = new Student(`Jmeno${i}`, `Prijmeni${i}`, 18);
+    classB.pridejStudenta(student);
+}
+
+console.log("Studenti třídy 4A:");
+classA.studenti.forEach(student => console.log(student.toString()));
+
+console.log("\nStudenti třídy 4B:");
+classB.studenti.forEach(student => console.log(student.toString()));
 
 
